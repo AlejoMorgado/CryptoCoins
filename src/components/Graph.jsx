505abd444974b852eb46aeb4081ddbba2ebd4514
Alejandro Morgado <alejo.morgado2@gmail.com>
2023-05-31 17:00:41 -0500

    Fixed issues

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -26,7 +26,38 @@ function ChartDisplay() {
         {historyData && (
           <Bar
             className='chart'
-
+            data={{
+              labels: historyData.prices.map(([timestamp]) =>
+                new Date(timestamp).toLocaleDateString()
+              ),
+              datasets: [
+                {
+                  data: historyData.prices.map(([, price]) => price),
+                  backgroundColor: 'rgba(104,107,127,255)',
+                  borderColor: 'rgba(104,107,127,255)',
+                  hoverBackgroundColor: 'rgba(194,238,10,255)',
+                  hoverBorderColor: 'rgba(194,238,10,255)', 
+                  fill: true,
+                },
+              ],
+            }}
+            options={{
+              indexAxis: 'x', 
+              plugins: {
+                legend: {
+                  display: false, 
+                },
+              },
+              scales: {
+                y: {
+                  display: false,
+                  beginAtZero: true,
+                  ticks: {
+                    display: false,
+                  },
+                },
+              },
+            }}
           />
         )}
       </div>
