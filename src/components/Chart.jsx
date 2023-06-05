e8e792dc11c663a4d8b02f4c333b2a0f1a893bd5
Alejandro Morgado <alejo.morgado2@gmail.com>
2023-06-05 11:19:50 -0500

    Delete the folders

---
+++ src/components/Chart.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Chart = () => {
+  return (
+    <div>
+                  data={{
+              labels: historyData.prices.map(([timestamp]) =>
+                new Date(timestamp).toLocaleDateString()
+              ),
+              datasets: [
+                {
+                  data: historyData.prices.map(([, price]) => price),
+                  backgroundColor: 'rgba(104,107,127,255)',
+                  borderColor: 'rgba(104,107,127,255)',
+                  hoverBackgroundColor: 'rgba(194,238,10,255)',
+                  hoverBorderColor: 'rgba(194,238,10,255)', 
+                  fill: true,
+                },
+              ],
+            }}
+            options={{
+              indexAxis: 'x', 
+              plugins: {
+                legend: {
+                  display: false, 
+                },
+              },
+              scales: {
+                y: {
+                  display: false,
+                  beginAtZero: true,
+                  ticks: {
+                    display: false,
+                  },
+                },
+              },
+            }}
+    </div>
+  )
+}
+
+export default Chart
